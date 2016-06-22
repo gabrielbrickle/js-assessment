f7f4ad6bfefda580584f3906902cc561786054ff
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-06-22 18:42:32 -0400

    14 passes so far

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,94 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+    indexOf: function(arr, item) {
+        return arr.indexOf(item);
+        ///or
+        // for ( var i = 0; i < arr.length; i++ ){
+        //   if (arr[i]===index){
+        //     return i;
+        //   }
+        // }
+    },
 
-  },
+    sum: function(arr) {
+        var total = 0;
+        for (var i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total
+    },
 
-  sum: function(arr) {
+    remove: function(arr, item) { ////NOT WORKING
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i--, 1);
+            }
+        }
+        return arr;
+    },
 
-  },
+    removeWithoutCopy: function(arr, item) { //NOT WORKING
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i--, 1);
+            }
+        }
+        return arr;
+    },
 
-  remove: function(arr, item) {
+    append: function(arr, item) {
+        arr.push(item);
+        return arr;
 
-  },
+    },
 
-  removeWithoutCopy: function(arr, item) {
+    truncate: function(arr) {
+        arr.pop();
+        return arr;
 
-  },
+    },
 
-  append: function(arr, item) {
+    prepend: function(arr, item) {
+        arr.unshift(item);
+        return arr;
+    },
 
-  },
+    curtail: function(arr) {
+        arr.shift();
+        return arr;
+    },
 
-  truncate: function(arr) {
+    concat: function(arr1, arr2) {
+        var arr3 = arr1.concat(arr2);
+        return arr3;
+    },
 
-  },
+    insert: function(arr, item, index) {
+        arr.splice(index, 0, item);
+        return arr;
+    },
 
-  prepend: function(arr, item) {
+    count: function(arr, item) { ///not working
+        arr.filter(function(element) {
+            if (element === item) {
+                return element.length;
+            }
+        })
+        return arr;
+    },
 
-  },
+    duplicates: function(arr) {
 
-  curtail: function(arr) {
 
-  },
+    },
 
-  concat: function(arr1, arr2) {
+    square: function(arr) {
+        Math.pow(arr[index], 2);
+        return arr;
+    },
 
-  },
+    findAllOccurrences: function(arr, target) {
 
-  insert: function(arr, item, index) {
-
-  },
-
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+    }
 };
--- app/objects.js
+++ app/objects.js
@@ -2,11 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+      
   },
 
   alterObjects: function(constructor, greeting) {
 
+  }
   },
 
   iterate: function(obj) {
